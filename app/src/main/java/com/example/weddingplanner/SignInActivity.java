13496ba988be11cb89258b7752595c464f969784
Santhosh Nallapati <santhoshnallapati@santhoshslaptop.home>
2024-06-22 23:33:58 -0400

    signin activity created

--- app/src/main/java/com/example/weddingplanner/SignInActivity.java
+++ app/src/main/java/com/example/weddingplanner/SignInActivity.java
@@ -1,20 +1,92 @@
 package com.example.weddingplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignInActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+    Button btn_signIn, btn_register;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+
+        editTextEmail = findViewById(R.id.etEmail);
+        editTextPassword = findViewById(R.id.etPassword);
+        btn_signIn = findViewById(R.id.btnSignIn);
+        btn_register = findViewById(R.id.btnRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(SignInActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(SignInActivity.this, "Sign in success", Toast.LENGTH_SHORT).show();
+                            goHome(email);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SignInActivity.this, "Sign in failed try again", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void goHome(String email){
+        Intent intent;
+        if (email.equals("admin@gmail.com")) {
+            intent = new Intent(SignInActivity.this, AdminActivity.class);
+
+        }else {
+            intent = new Intent(SignInActivity.this, MainActivity.class);
+
+        }
+
+        startActivity(intent);
+        finish();
+
 
 
     }
