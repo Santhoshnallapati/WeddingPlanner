91ad3db1606a3a0745654a2385ec2f039c8fb181
Santhosh Nallapati <santhoshnallapati@Santhoshs-Laptop.local>
2024-08-17 13:27:36 -0400

    back button created

--- app/src/main/java/com/example/weddingplanner/EditProfileActivity.java
+++ app/src/main/java/com/example/weddingplanner/EditProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.weddingplanner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference mUsersDatabase;
+    private String customerId;
+
+    private EditText editTextUsername;
+    private Button buttonSave, buttonBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        buttonBack = findViewById(R.id.btnBack);
+        buttonSave = findViewById(R.id.btnSaveAccount);
+
+
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 }
